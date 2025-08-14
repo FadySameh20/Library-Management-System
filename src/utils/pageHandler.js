33613728cdddd4131e3fc3c5f4699ebec3bbcc32
FadySameh20 <fadysamehmishriki@gmail.com>
2025-08-14 22:03:17 +0300

    Return page number in the metadata as number instead of string

--- src/utils/pageHandler.js
+++ src/utils/pageHandler.js
@@ -2,10 +2,14 @@ import { PAGINATION } from "../constants/constants.js";
 import { isPositiveInteger } from "./validators.js";
 
 export const getPageInfo = (req) => {
-  const { page: requestedPage, pageSize: requestedPageSize } = req.query;
-  const page = isPositiveInteger(Number(requestedPage)) ? requestedPage : PAGINATION.DEFAULT_PAGE_NUMBER;
-  const pageSize = isPositiveInteger(Number(requestedPageSize)) ? Math.min(requestedPageSize, PAGINATION.MAX_PAGE_SIZE) : PAGINATION.DEFAULT_PAGE_SIZE;
-  return { page, pageSize };
+  const { page, pageSize } = req.query;
+  const requestedPage = Number(page);
+  const requestedPageSize = Number(pageSize);
+  
+  const pagination = {}
+  pagination.page = isPositiveInteger(requestedPage) ? requestedPage : PAGINATION.DEFAULT_PAGE_NUMBER;
+  pagination.pageSize = isPositiveInteger(requestedPageSize) ? Math.min(requestedPageSize, PAGINATION.MAX_PAGE_SIZE) : PAGINATION.DEFAULT_PAGE_SIZE;
+  return pagination;
 }
 
 export const getTotalPages = (total, pageSize) => Math.max(1, Math.ceil(total / pageSize));
